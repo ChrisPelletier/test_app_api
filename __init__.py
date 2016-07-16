83e06baa544336901b700315ca1fe33c6716d800
ChrisPelletier <chris.r.pelletier@gmail.com>
2016-07-16 08:56:18 -0700

    Added cars and admin stuff

--- __init__.py
+++ __init__.py
@@ -2,8 +2,12 @@ from flask import Flask, jsonify, abort, make_response, request, url_for
 from mongokit import Connection
 from sample_app.models.user import User
 import sample_app.config as config
+from flask_cors import CORS, cross_origin
+from flask_admin import Admin
+
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['DEBUG'] = config.DEBUG
 app.config['MONGODB_HOST'] = config.MONGODB_HOST
@@ -11,6 +15,8 @@ app.config['MONGODB_HOST'] = config.MONGODB_HOST
 
 connection = Connection(app.config['MONGODB_HOST'])
 connection.register([User])
+
+admin = Admin(app, name='Sample App', template_mode='bootstrap3')
 
 @app.errorhandler(404)
 def not_found(error):
